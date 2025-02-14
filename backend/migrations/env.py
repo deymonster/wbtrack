480a979c6af40c2a2288a6cee26b71234b6e6824
deymonster <deymonster@yandex.ru>
2025-02-14 15:42:01 +0500

    update alembic ini

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -18,7 +18,9 @@ from config import settings
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
 config.set_main_option("sqlalchemy.url", settings.DB_ASYNC_CONNECTION_STR)
+
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
