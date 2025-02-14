e07343a25eeff61a447faabcca0f944158c4d95f
deymonster <deymonster@yandex.ru>
2025-02-14 13:17:43 +0500

    add log to test

--- backend/config.py
+++ backend/config.py
@@ -48,8 +48,11 @@ class Settings(BaseSettings):
     @property
     def REDIS_URL(self) -> str:
         host = self.REDIS_HOST if self.ENVIRONMENT == "production" else "localhost"
+        
         if self.REDIS_PASSWORD:
-            return f"redis://default:{self.REDIS_PASSWORD}@{host}:{self.REDIS_PORT}/0"
+            url = f"redis://default:{self.REDIS_PASSWORD}@{host}:{self.REDIS_PORT}/0"
+            print(f"Generated REDIS_URL": {url})
+            return url
         return f"redis://{host}:{self.REDIS_PORT}/0"
         # return str(
         #     RedisDsn.build(  # type: ignore
