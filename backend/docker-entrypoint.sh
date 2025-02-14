81907f51fa361b5cf92845d1e8680df0602c3a1a
deymonster <deymonster@yandex.ru>
2025-02-14 15:09:55 +0500

    remove docker entry point

--- backend/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-echo "Applying database migrations..."
-alembic upgrade head --verbose
-
-echo "Starting FastAPI application..."
-exec uvicorn asgi:app --host 0.0.0.0 --port 8000 --workers 4 --proxy-headers
