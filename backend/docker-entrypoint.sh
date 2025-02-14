92f0ecf4966e4ab671b7576063a07bcdd4de00b8
deymonster <deymonster@yandex.ru>
2025-02-14 15:01:47 +0500

    update docker entrypoint

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Applying database migrations..."
-alembic upgrade head
+alembic upgrade head --verbose
 
 echo "Starting FastAPI application..."
 exec uvicorn asgi:app --host 0.0.0.0 --port 8000 --workers 4 --proxy-headers
